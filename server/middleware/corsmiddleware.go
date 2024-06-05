35bf336bfd37170fa21866a9293648addd0703ac
vivek_2401 <vivek.m@iqm.com>
2024-06-05 10:21:19 +0530

    added yml file for golang

--- server/middleware/corsmiddleware.go
+++ server/middleware/corsmiddleware.go
@@ -6,7 +6,7 @@ import (
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Disposition")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
 
